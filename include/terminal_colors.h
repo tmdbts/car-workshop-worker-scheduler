5103b3ee19b4887b454530304ddde372da9e6cee
Tiago Silva <tbrancosilva@hotmail.com>
2023-05-24 17:13:32 +0100

    ✨ implemented ANSI terminal colors

---
+++ include/terminal_colors.h
@@ -0,0 +1,44 @@
+#ifndef CAR_WORKSHOP_WORKER_SCHEDULER_TERMINAL_COLORS_H
+#define CAR_WORKSHOP_WORKER_SCHEDULER_TERMINAL_COLORS_H
+
+/**
+ * Stores the ANSI code for the default color
+ */
+extern const char *TERMINAL_COLOR_DEFAULT;
+
+/**
+ * Stores the ANSI code for the color red
+ */
+extern const char *TERMINAL_COLOR_RED;
+
+/**
+ * Stores the ANSI code for the color green .
+ */
+extern const char *TERMINAL_COLOR_GREEN;
+
+/**
+ * Stores the ANSI code for the color yellow
+ */
+extern const char *TERMINAL_COLOR_YELLOW;
+
+/**
+ * Stores the ANSI code for the color blue
+ */
+extern const char *TERMINAL_COLOR_BLUE;
+
+/**
+ * Stores the ANSI code for the color magenta
+ */
+extern const char *TERMINAL_COLOR_MAGENTA;
+
+/**
+ * Stores the ANSI code for the color cyan
+ */
+extern const char *TERMINAL_COLOR_CYAN;
+
+/**
+ * Stores the ANSI code for the color white
+ */
+extern const char *TERMINAL_COLOR_WHITE;
+
+#endif //CAR_WORKSHOP_WORKER_SCHEDULER_TERMINAL_COLORS_H
---
+++ src/constants/terminal_colors.c
@@ -0,0 +1,17 @@
+#include "../../include/terminal_colors.h"
+
+const char *TERMINAL_COLOR_DEFAULT = "\x1B[0m";
+
+const char *TERMINAL_COLOR_RED = "\x1B[31m";
+
+const char *TERMINAL_COLOR_GREEN = "\x1B[32m";
+
+const char *TERMINAL_COLOR_YELLOW = "\x1B[33m";
+
+const char *TERMINAL_COLOR_BLUE = "\x1B[34m";
+
+const char *TERMINAL_COLOR_MAGENTA = "\x1B[35m";
+
+const char *TERMINAL_COLOR_CYAN = "\x1B[36m";
+
+const char *TERMINAL_COLOR_WHITE = "\x1B[37m";
