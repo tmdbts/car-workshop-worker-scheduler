f5c7a99b098841313b667abbb5d0dd8cbb24716b
Tiago Silva <tbrancosilva@hotmail.com>
2023-05-25 19:27:58 +0100

    ✨ implement binary input reader

--- include/terminal.h
+++ include/terminal.h
@@ -3,10 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include "terminal_colors.h"
 
 /**
  * Clears the terminal
  */
 void clear();
+
+bool readYesNoInput();
 
 #endif //CAR_WORKSHOP_WORKER_SCHEDULER_TERMINAL_H
--- lib/terminal.c
+++ lib/terminal.c
@@ -9,3 +9,18 @@ void clear() {
 
     system("clear");
 }
+
+bool readYesNoInput() {
+    char input[2];
+
+    while (true) {
+        fgets(input, sizeof(input), stdin);
+
+        if (input[0] == '\n') return true;
+
+        if (input[0] == 'y' || input[0] == 'Y') return true;
+        if (input[0] == 'n' || input[0] == 'N') return false;
+
+        printf("%sInvalid input. (Y/n) ", TERMINAL_COLOR_YELLOW);
+    }
+}
